470f7bac4777837f91cdbba2ade9e9a8c7a8406e
Marek Kochanowski <marek@kochanow.ski>
2019-05-21 20:00:53 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="Maisie-Sphinx-Theme",
-    version="0.1.0",
+    version="0.1.1",
     description="Sphinx theme used by Maisie.",
     long_description=open("README.rst").read(),
     author="Marek Kochanowski",
